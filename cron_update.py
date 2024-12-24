3dae00eed5dc1417486d93f9128ef3ca1396b3f1
fenixer <143337992+Fenixer@users.noreply.github.com>
2024-12-24 16:03:09 +0000

    Auto Commit: Added ch

--- cron_update.py
+++ cron_update.py
@@ -18,6 +18,8 @@ def update_chapter():
     rss_raw = BytesIO(requests.get(feed_url, impersonate="chrome").content)
     rss = feedparser.parse(rss_raw)
 
+    chapters = []
+
     for i in range(0, 5):
         link_etc = rss['entries'][i]['content'][0]['value']
 
@@ -28,53 +30,54 @@ def update_chapter():
         else:
             continue
 
-    if not chapter_code:
-        print("No new chapter")
-        return
+        if not chapter_code:
+            continue
 
-    print(f"Imgur link: https://cubari.moe/read/imgur/{chapter_code}")
-    
-    # title = requests.get(f"https://cubari.moe/read/api/imgur/series/{chapter_code}/").json()['title']
+        print(f"Imgur link: https://cubari.moe/read/imgur/{chapter_code}")
+        
+        # title = requests.get(f"https://cubari.moe/read/api/imgur/series/{chapter_code}/").json()['title']
 
-    regex = r"Chapter (\d+) ?[.|\-:!?]? ? [.|:!?]?([^.|!?]*)"
-
-
-    matches = re.search(regex, link_etc)
-    chapter_number = eval(re.search(r"Chapter (\d+)", link_etc, flags=re.IGNORECASE).group(1).strip())
-    try:
-        chapter_title = matches.group(2).replace("'", "").replace('"', "").strip()
-    except:
-        chapter_title = f"Chapter {chapter_number}"
-    vol = volume or chapter_number/10
-
-    with open('chapters.json', 'r', encoding="utf8") as file:
-        data = json.load(file)
-
-    if str(chapter_number) in list(data['chapters'].keys()):
-        print(f"Chapter {chapter_number} already exists")
-        return
+        regex = r"Chapter (\d+) ?[.|\-:!?]? ? [.|:!?]?([^.|!?]*)"
 
 
-    data['chapters'][chapter_number] = {
-        'title': chapter_title,
-        'volume': vol,
-        'last_updated': int(time.time()),
-        'groups': {
-            '': f'/proxy/api/imgur/chapter/{chapter_code}/'
+        matches = re.search(regex, link_etc)
+        chapter_number = eval(re.search(r"Chapter (\d+)", link_etc, flags=re.IGNORECASE).group(1).strip())
+        try:
+            chapter_title = matches.group(2).replace("'", "").replace('"', "").strip()
+        except:
+            chapter_title = f"Chapter {chapter_number}"
+        vol = volume or chapter_number/10
+
+        with open('chapters.json', 'r', encoding="utf8") as file:
+            data = json.load(file)
+
+        if str(chapter_number) in list(data['chapters'].keys()):
+            print(f"Chapter {chapter_number} already exists")
+            return
+
+
+        data['chapters'][chapter_number] = {
+            'title': chapter_title,
+            'volume': vol,
+            'last_updated': int(time.time()),
+            'groups': {
+                '': f'/proxy/api/imgur/chapter/{chapter_code}/'
+            }
         }
-    }
 
-    with open('chapters.json', 'w') as file:
-        json.dump(data, file, indent=4)
+        with open('chapters.json', 'w') as file:
+            json.dump(data, file, indent=4)
 
-    print(f"Updated chapter {chapter_number}")
+        print(f"Updated chapter {chapter_number}")
+        chapters.append(chapter_number)
 
+    chapter_number = ", ch".join(chapters)
 
     # Create a Github instance
     os.system("git config --local user.name fenixer")
     os.system("git config --local user.email 143337992+Fenixer@users.noreply.github.com")
     os.system("git add .")
-    os.system(f'git commit -m "Auto Commit: Added {chapter_number}"')
+    os.system(f'git commit -m "Auto Commit: Added ch{chapter_number}"')
     os.system("git push")
 
     # g = Github(os.getenv("UPDATE_TOKEN"))
